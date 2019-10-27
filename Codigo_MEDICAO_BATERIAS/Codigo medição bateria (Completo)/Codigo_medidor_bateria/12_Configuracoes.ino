45ae3ee2ff7351479e0bce211fe1e4f98144fdfe
Elismar13 <elismarsilva.5@outlook.com>
2019-10-27 09:03:15 -0300

    Função para escrever no cartão atualizada

---
+++ Codigo_MEDICAO_BATERIAS/Codigo medição bateria (Completo)/Codigo_medidor_bateria/12_Configuracoes.ino
@@ -0,0 +1,405 @@
+//************************** FUNÇÕES PARA O MENU DE CONFIGURAÇÕES **********************************
+void configuracoes() {
+    while(!digitalRead(up) == LOW) {          //Loop necessário para o programa ficar nesse menu
+      lcd.clear();
+      //Printar as palavras
+      lcd.setCursor(2, 0);
+      lcd.print("Configuracoes");
+
+      if(SeletorConfiguracoes < 2) {
+        lcd.setCursor(1, 1);
+        lcd.print("Arquivo");
+        lcd.setCursor(12, 1);
+        lcd.print("Hora");
+      }
+
+      if(!digitalRead(mais) == HIGH) {
+        SeletorConfiguracoes += 1;
+        delay(AtrasoBotoes);
+      } 
+      else if(!digitalRead(menos) == HIGH) {
+        SeletorConfiguracoes -= 1; 
+        delay(AtrasoBotoes);
+      }
+    
+      //Limites para o menu de configuracoes
+      if(SeletorConfiguracoes > 2){
+        SeletorConfiguracoes = 0;
+      }
+      else if(SeletorConfiguracoes < 0) {
+        SeletorConfiguracoes = 0;
+      }
+    
+      if(SeletorConfiguracoes == 0) {
+          lcd.setCursor(0, 1);
+          lcd.print(">");
+          lcd.setCursor(11, 1);
+          lcd.write(' ');
+      }
+      else if(SeletorConfiguracoes == 1) {
+          lcd.setCursor(0, 1);
+          lcd.print(' ');
+          lcd.setCursor(11, 1);
+          lcd.write('>');
+      }
+      else {
+          lcd.setCursor(0, 1);
+          lcd.write('>');
+          lcd.print("Calibrar ADC");
+      }
+
+      if(!digitalRead(select) == HIGH) {
+        delay(AtrasoBotoes);
+        switch(SeletorConfiguracoes) {
+          case 0:
+            configurarArquivo();
+            break;
+          case 1:
+            mostrarHora();
+            break;
+          case 2: 
+            calibracao();
+            break;
+        }
+      }
+     delay(AtualizacaoDisplay);     //Atraso para Atualizar o display
+    }
+}
+
+void configurarArquivo() {
+  int Horas = 0;
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  //lcd.print("Atualiza arquivo");
+  lcd.write('A');
+  lcd.write('t');
+  lcd.write('u');
+  lcd.write('a');
+  lcd.write('l');
+  lcd.write('i');
+  lcd.write('z');
+  lcd.write('a');
+  lcd.write(' ');
+  lcd.write('a');
+  lcd.write('r');
+  lcd.write('q');
+  lcd.write('u');
+  lcd.write('i');
+  lcd.write('v');
+  lcd.write('o');
+
+  //lcd.print("a cada: ");
+  lcd.setCursor(0, 1);
+  lcd.write('a');
+  lcd.write(' ');
+  lcd.write('c');
+  lcd.write('a');
+  lcd.write('d');
+  lcd.write('a');
+  lcd.write(':');
+  lcd.write(' ');
+
+
+  while(true) {
+    delay(AtualizacaoDisplay);     //Atraso para Atualizar o display
+
+    if(!digitalRead(select) == HIGH) {
+      delay(AtrasoBotoes);
+      break;
+    }
+    if(!digitalRead(menos) == HIGH) {
+      delay(AtrasoBotoes*2);
+      TempoParaSalvar--;
+      /*
+      while(!digitalRead(menos) == HIGH) {
+        TempoParaSalvar-= 5;
+        delay(AtrasoBotoes); 
+      }
+      */
+    }
+    if(!digitalRead(mais) == HIGH) {
+      delay(AtrasoBotoes*2);
+      TempoParaSalvar++;
+      /*
+      while(!digitalRead(mais) == HIGH) {
+        TempoParaSalvar += 5;
+        delay(AtrasoBotoes); 
+      }
+      */
+    }
+    if(TempoParaSalvar < 1) {
+      TempoParaSalvar = 240;
+    }
+    else if(TempoParaSalvar > 240) {
+      TempoParaSalvar = 1;
+    }
+
+    lcd.setCursor(9, 1);
+    Horas = (int)TempoParaSalvar/60;
+    lcd.write('0');
+
+    if(Horas == 0) {
+      lcd.write('0');
+    }
+    else if(Horas == 1){
+      lcd.write('1');
+    }
+    else if(Horas == 2) {
+      lcd.write('2');
+    }
+    else if(Horas == 3) {
+      lcd.write('3');
+    }
+    else if(Horas == 4){
+      lcd.write('4');
+    }
+    lcd.write('h');
+    
+    if(TempoParaSalvar < 60) {
+      if(TempoParaSalvar-(60*Horas) < 10) {
+        lcd.write('0');
+      }
+      lcd.print(TempoParaSalvar);
+    }
+    else if(TempoParaSalvar >= 60 && TempoParaSalvar < 120) {
+      if(TempoParaSalvar-(60*Horas) < 10) {
+        lcd.write('0');
+      }
+      lcd.print(TempoParaSalvar-60);
+    }
+    else if(TempoParaSalvar >= 120 && TempoParaSalvar < 180) {
+      if(TempoParaSalvar-(60*Horas) < 10) {
+        lcd.write('0');
+      }
+      lcd.print(TempoParaSalvar-120);
+    }
+    else if(TempoParaSalvar >= 180 && TempoParaSalvar < 240) {
+      if(TempoParaSalvar-(60*Horas) < 10) {
+        lcd.write('0');
+      }    
+      lcd.print(TempoParaSalvar-180);
+    }
+    else if (TempoParaSalvar >= 240) {
+      if(TempoParaSalvar-(60*Horas) < 10) {
+        lcd.write('0');
+      }
+      lcd.print(TempoParaSalvar - 240);
+    }
+    lcd.write('m');
+  }
+  EEPROM.update(EnderecoTempoSalvar, TempoParaSalvar);      //Atualizo o intervalo entre cada vez que o conteúdo é salvo.
+  salvoComSucesso();
+  return;
+}
+
+void mostrarHora() {
+  bool ManterMenu = false;
+  bool AlterarHora = true;
+  //String DadoDoTempo = "";
+  
+  lcd.clear();
+  delay(300);
+
+  while(!ManterMenu) {
+    LeituraHora(false);
+    lcd.setCursor(1 ,0);
+    lcd.print("Hor");
+    lcd.write((byte)5);
+    lcd.print("rio atual:");
+    lcd.setCursor(0, 1);
+    String DataHora = "";
+    
+    if(hora < 10) {               //Escrever a hora no display
+      DataHora += '0';
+    }
+    DataHora += String(hora);
+    DataHora += ':';
+       
+    if(minuto < 10) {             //Escrever o minuto no display
+      DataHora += '0';
+    }
+    DataHora += String(minuto);
+    DataHora += '-';
+
+    if(dia < 10) {                //Escrever o dia no display
+      DataHora += '0';
+    }
+    DataHora += String(dia); 
+    DataHora += ':';
+  
+    if(mes < 10) {                 //escrever o mês no LCD
+      DataHora += '0';      
+    }
+    DataHora += String(mes);
+    DataHora += ":20";
+
+    if(ano < 10) {
+      DataHora += '0';
+    }
+    DataHora += String(ano);
+
+    //Por fim, imprimo a data no display
+    lcd.print(DataHora);
+    
+    if(!digitalRead(select) == HIGH) {
+      ManterMenu = true;
+      delay(AtrasoBotoes);
+    }
+    if(!digitalRead(up) == HIGH) {
+      ManterMenu = false;
+      delay(AtrasoBotoes);
+      return;  
+    }
+    delay(AtualizacaoDisplay);     //Atraso para Atualizar o display
+  }
+
+  //Após verificar a hora, pergunto ao usuário se ele deseja alterar a hora
+  lcd.clear();
+  //Reaproveito a variável "ManterMenu" dessa vez no proxímo menu
+  ManterMenu = false;              
+  
+  while(!ManterMenu) {
+    lcd.setCursor(0, 0);
+    lcd.print("Configurar Data?");
+    
+    lcd.setCursor(3, 1);
+    lcd.print("SIM");
+    lcd.setCursor(10, 1);
+    lcd.write('N');
+    lcd.write((byte)4);
+    lcd.write('O');
+    
+    if(AlterarHora) {
+      lcd.setCursor(2, 1);
+      lcd.write('>');
+      lcd.setCursor(9, 1);
+      lcd.write(' ');
+    }
+    else {
+      lcd.setCursor(2, 1);
+      lcd.write(" ");
+      lcd.setCursor(9, 1);
+      lcd.write(">");
+    }
+
+    if((!digitalRead(mais) == HIGH) || (!digitalRead(menos) == HIGH)) {
+      AlterarHora = !AlterarHora;
+      delay(AtrasoBotoes);
+    }
+    if(!digitalRead(select) == HIGH) {
+      ManterMenu = true;
+      delay(AtrasoBotoes);
+    }
+    delay(AtualizacaoDisplay);     //Atraso para Atualizar o display
+  }
+  lcd.clear();
+
+  
+  if(AlterarHora) {
+    funcaoAlterarHora();
+  }
+}
+
+
+//Função para calibrar o ADC
+void calibracao() {
+  float offset_Calibrado = 0;     //Variável auxiliar para mostrar o offset no display
+  CalibracaoADC = EEPROM.read(EnderecoCalibracao);
+  //CalibracaoADC = 20;
+  
+  while (true) {
+    lcd.clear();
+  
+    //"Constante de calibracao: XX.XX"
+    offset_Calibrado = 0;
+    lcd.setCursor(2, 0);
+    lcd.write('O');
+    lcd.write('f');
+    lcd.write('f');
+    lcd.write('s');
+    lcd.write('e');
+    lcd.write('t');
+    lcd.write(' ');
+    lcd.write('d');
+    lcd.write('e');
+    //lcd.print(CalibracaoADC);
+  
+    lcd.setCursor(0, 1);
+    lcd.write('c');
+    lcd.write('a');
+    lcd.write('l');
+    lcd.write('i');
+    lcd.write('b');
+    lcd.write('r');
+    lcd.write('a');
+    lcd.write((byte)2);
+    lcd.write((byte)4);
+    lcd.write('o');
+    lcd.write(':');
+    if(CalibracaoADC >= 20) lcd.write(' ');
+     
+    if(CalibracaoADC < 20) {
+      for(byte i = CalibracaoADC; i < 20; i++) {
+        offset_Calibrado -= 0.01;
+      }
+    }
+    else if (CalibracaoADC > 20) {
+      for(byte i = 21; i <= CalibracaoADC; i++) {
+        offset_Calibrado += 0.01;
+      }
+    }
+    else {
+      offset_Calibrado = 0.00f;
+    }
+    
+    lcd.print(offset_Calibrado); 
+  
+    //Leitura botoes 
+    if(!digitalRead(mais) == HIGH) {
+      CalibracaoADC += 1;
+      delay(AtrasoBotoes*2);
+    }
+    if(!digitalRead(menos) == HIGH) {
+      CalibracaoADC -= 1;
+      delay(AtrasoBotoes*2);
+    }
+    if(!digitalRead(select) == HIGH) {
+      delay(AtrasoBotoes);
+      break;
+    }
+    if(!digitalRead(up) == HIGH) {
+      delay(AtrasoBotoes*4);
+      return;
+    }
+  
+    //Limites 
+    if(CalibracaoADC < 0) {
+      CalibracaoADC = 40;
+    }
+    else if(CalibracaoADC > 40) {
+      CalibracaoADC = 0;
+     }
+
+    delay(AtualizacaoDisplay);
+  }
+
+
+  //Se o usuário decidiu ficar neste menu, não seja por isso, atualizo a o valor da calibragem.
+  if(CalibracaoADC != 20) {
+    for(byte i = 0; i<(abs(CalibracaoADC-20)); i++) {
+      if(CalibracaoADC < 20) {
+        offset += 0.01;
+      }
+      else if (CalibracaoADC > 20) {
+       offset -= 0.01;
+      }
+      else {
+        offset = 0.0f;
+      }
+    }
+    offset = offset_Calibrado;
+  }
+  EEPROM.update(EnderecoCalibracao, CalibracaoADC);
+  salvoComSucesso();
+}
---
+++ Codigo_MEDICAO_BATERIAS/Codigo medição bateria (Completo)/Codigo_medidor_bateria/Codigo_medidor_bateria.ino
@@ -0,0 +1,211 @@
+//===================================== Bibliotecas E Definições =====================================
+#include <EEPROM.h>         //EEPROM
+#include <SPI.h>            //Biblioteca necessária para comunicação SPI
+#include <SD.h>             //Biblioteca necessária para comunicação SD card
+#include <Wire.h>           //Biblioteca para comunicação I2C com módulo RTC
+#include <LiquidCrystal.h>  //Biblioteca para display 16x2
+//#include <avr/wdt.h>        //Biblioteca referente ao Timer WatchDog
+
+//============================================= HARDWARE =============================================
+//Constantes
+#define DS1307_ADDRESS      0x68
+#define AtualizacaoDisplay  100
+#define AtrasoBotoes        190
+#define EnderecoTempoSalvar 0
+#define EnderecoCalibracao  1
+
+//Pinos
+#define rs          2       //D2
+#define en          3       //D3
+#define d4          4       //D4
+#define d5          5       //D5
+#define d6          6       //D6
+#define d7          7       //D7
+
+#define up          8       //D8
+#define select      9       //D9
+#define mais        0       //D0
+#define menos       1       //D1
+
+#define SelecaoChip 10      //D10 -Chip Select
+#define MOSI        11      //D11 - MOSI
+#define MISO        12      //D12 - MISO
+#define SCK         13      //D13 - SCk
+
+#define Bateria_1   14      //A0
+#define Bateria_2   15      //A1
+#define Bateria_3   16      //A2
+#define Bateria_4   17      //A3
+
+//============================================= Variáveis ============================================
+LiquidCrystal lcd (rs, en, d4, d5, d6, d7); //Crio o objeto lcd com seus respectivos pinos
+const byte SelecionadorOpcao[8] =                 //Lista para o caractere especial (O quadrado)
+{
+  0b00000,
+  0b00000,
+  0b01110,
+  0b01110,
+  0b01110,
+  0b01110,
+  0b00000,
+  0b00000
+};
+
+//Caracteres especiais
+  const byte C_Cedilhado[8] {                       //Lista para o caractere especial ("Ç")
+    0b00000,
+    0b01110,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b01110,
+    0b00100,
+    0b00100
+  };
+  
+  const byte O_Til[8] {                             //Lista para o caractere especial ("Õ")
+    0b01110,
+    0b00000,
+    0b01110,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b01110,
+    0b00000
+  };
+  
+  const byte A_Til[8] {                             //Lista para o caractere especial ("Ã")
+    0b01110,
+    0b00000,
+    0b01110,
+    0b00001,
+    0b01111,
+    0b10001,
+    0b01111,
+    0b00000
+  };
+
+  const byte A_Agudo[8] {                             //Lista para o caractere especial ("Ã")
+    0b00010,
+    0b00100,
+    0b01110,
+    0b00001,
+    0b01111,
+    0b10001,
+    0b01111,
+    0b00000
+  };
+  
+//Seletores para os menus
+byte SeletorPrincipal = 0;
+byte SeletorDadosBateria = 1;
+byte SeletorConfiguracoes = 0;
+
+//De tempo
+byte Timer_salvar = 0;
+byte HoraAnterior_salvar = 0;
+byte MinutoAnterior_salvar = 0;
+byte TempoParaSalvar = 1;
+
+//Calibracao do filtro ADC
+byte CalibracaoADC = 20;    //de 0 à 40. 20 Seria é o equivalente de '0' 
+const float TensaoADC = 5.00f;
+float offset = 0.00f;
+
+//Número de amostras para o filtro ADC. Quanto maior, melhor, mas terá impacto no desempenho do sistema.
+byte Amostras_Filtro = 30;
+
+//Das baterias
+float Bateria1 = 0.00f;
+float Bateria2 = 0.00f;
+float Bateria3 = 0.00f;
+float Bateria4 = 0.00f;
+//float Teste[20] = {0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f};
+
+
+//Do relogio RTC
+  byte segundo = 0;               //0~59
+  byte minuto = 0;                //0~59
+  byte hora = 0;                  //Formato 24 horas
+  byte diasemana = 0;             //0-6 -> Domingo - Sábado
+  byte dia = 0;                   //Dependendo do mês, de 1~31
+  byte mes = 0;                   //1~12
+  byte ano = 0;                   //0~99
+
+//=================================== Inicio do Programa ==========================================
+void setup() {
+  Wire.begin();
+  /*     DEBUG  
+  Serial.begin(9600);
+  */
+  //Botões de interação
+  pinMode(up, INPUT_PULLUP);
+  pinMode(mais, INPUT_PULLUP);
+  pinMode(menos, INPUT_PULLUP);
+  pinMode(select, INPUT_PULLUP);
+
+  //Pinos ADC para as baterias
+  pinMode(Bateria_1, INPUT);
+  pinMode(Bateria_2, INPUT);
+  pinMode(Bateria_3, INPUT);
+  pinMode(Bateria_4, INPUT);
+
+  //pinMode(en, OUTPUT);
+
+  //Criação de caracteres especiais
+  lcd.createChar(1, SelecionadorOpcao);
+  lcd.createChar(2, C_Cedilhado);
+  lcd.createChar(3, O_Til);
+  lcd.createChar(4, A_Til);
+  lcd.createChar(5 , A_Agudo);
+
+  //Mensagem inicial display
+  lcd.clear();
+  lcd.begin(16, 2);
+  //Boas vindas
+  lcd.setCursor(2, 0);
+  lcd.print("Iniciando");
+  for(byte i = 0; i < 3; i++) {
+    lcd.write('.');
+    delay(1);
+  }
+
+  //Verifico e atualizo o tempo
+  if(EEPROM.read(EnderecoTempoSalvar) == 0) {
+    TempoParaSalvar = 1;
+    EEPROM.update(EnderecoTempoSalvar, 1); 
+  }
+  TempoParaSalvar = EEPROM.read(EnderecoTempoSalvar);
+
+
+  //Enquanto o cartão não for inserido eu mostro o erro                     
+  while(!SD.begin(SelecaoChip)) {
+     erroAbrirCartao();
+  }
+  
+  //Se for encontrado:
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Cartao SD ");
+  lcd.setCursor(2, 1);
+  lcd.print("encontrado.");  
+  delay(50);
+
+
+  //Verifico e atualizo a calibracao
+  //CalibracaoADC = EEPROM.read(EnderecoCalibracao);
+  if(CalibracaoADC != 20) {
+    for(byte i = 0; i < (abs(CalibracaoADC-20)); i++) {
+      if(CalibracaoADC > 20) {
+        offset += 0.001;
+      }
+      else {
+        offset -= 0.001;
+      }
+    }
+  }
+}
+
+void loop() {
+  menuPrincipal();
+}
